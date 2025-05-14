fc19fc192882e948caa3a123fdb65bc9a11b8cd6
DEDE4306 <Minjun_Chen430@outlook.com>
2025-05-14 20:23:08 +0800

    修改了部分后端，满足接口要求

--- backend_AI/app/rag/embedder.py
+++ backend_AI/app/rag/embedder.py
@@ -6,17 +6,20 @@ import torch
 
 # 用于将数据集向量化
 class Embedder:
+    _model = None
     def __init__(self, model_name='BAAI/bge-large-zh-v1.5', use_gpu=True):
-        print("Loading embedding model...")
         self.device = 'cuda' if use_gpu and torch.cuda.is_available() else 'cpu'
-        print('self.device = ' + self.device)
-        self.model = SentenceTransformer(model_name,device=self.device)
-        self.model.eval()
+        if Embedder._model is None:
+            print("Loading embedding model...")
+            print('self.device = ' + self.device)
+            Embedder._model = SentenceTransformer(model_name,device=self.device)
+            Embedder._model.eval()
+        self._model = Embedder._model.to(self.device)
 
     # 将数据集向量化
     def embed(self, texts):
         if isinstance(texts, str):
             texts = [texts]
-        embeddings = self.model.encode(texts, show_progress_bar=False, convert_to_tensor=True)
+        embeddings = self._model.encode(texts, show_progress_bar=False, convert_to_tensor=True)
         # print(embeddings.cpu().numpy())
         return embeddings.cpu().numpy()
